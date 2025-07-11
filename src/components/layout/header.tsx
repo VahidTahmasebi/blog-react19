3bb4db7f13907058f1863bdd0b31a2296ca1b953
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2025-07-11 16:50:00 +0330

    feat: add header page

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,64 @@
 "use client";
 
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+
 function Header() {
-  return <div></div>;
+  const router = useRouter();
+
+  const navItem = [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Create Post",
+      href: "post/create",
+    },
+  ];
+
+  return (
+    <div className="sticky top-0 z-10 border-b bg-background">
+      <div className="container h-16 flex items-center justify-between mx-auto px-4">
+        <div className="flex items-center gap-6">
+          <Link href="/" className="text-xl font-bold">
+            Next 15 Blog
+          </Link>
+          <nav className="hidden md:flex items-center gap-6">
+            {navItem.map((item) => (
+              <Link
+                key={item.label}
+                href={item.href}
+                className={cn(
+                  "text-sm font-medium transition-colors hover:text-primary",
+                )}
+              >
+                {item.label}
+              </Link>
+            ))}
+          </nav>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <div className="hidden md:block">{/* Search */}</div>
+
+          <div className="flex items-center gap-2">
+            <Button
+              asChild
+              variant="default"
+              className="cursor-pointer"
+              onClick={() => {
+                router.push("/auth");
+              }}
+            >
+              <Link href="/auth">Login</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Header;
