72c3630cd5b50299346a95d32e54d7c9289fb27b
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2025-07-11 16:50:46 +0330

    feat: add login and register forms

---
+++ src/components/auth/login-form.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const loginSchema = z.object({
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+type LoginFormValues = z.infer<typeof loginSchema>;
+
+function LoginForm() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onLoginSubmit = async (values: LoginFormValues) => {
+    setIsLoading(true);
+
+    try {
+    } catch (e) {}
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onLoginSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="Enter your email" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="password"
+                  placeholder="Enter your password"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" disabled={isLoading} className="w-full">
+          {isLoading ? "Singing in..." : "Sign In"}
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+export default LoginForm;
