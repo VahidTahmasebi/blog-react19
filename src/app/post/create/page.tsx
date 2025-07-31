8044e171bb3bae01fcadc8fa44c015b81e41482c
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2025-07-31 10:00:11 +0330

    feat: create post page

--- src/app/post/create/page.tsx
+++ src/app/post/create/page.tsx
@@ -1,5 +1,19 @@
+import PostForm from "@/components/post/post-form";
+import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 function CreatePostPage() {
-  return <div>CreatePostPage</div>;
+  return (
+    <main className="py-10">
+      <div className="max-w-4xl mx-auto">
+        <CardHeader>
+          <CardTitle className="text-4xl font-bold">Create New Post</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <PostForm />
+        </CardContent>
+      </div>
+    </main>
+  );
 }
 
 export default CreatePostPage;
